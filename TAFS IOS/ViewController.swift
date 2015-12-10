79483e3d768887e134f8527c5d2564f8b5a1d507
Moh Reza Luthfiansyah <Owls@mohs-mac-2.local>
2015-12-10 11:41:08 +0700

    Initial Commit

---
+++ TAFS IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TAFS IOS
+//
+//  Created by Moh Reza Luthfiansyah on 12/10/15.
+//  Copyright © 2015 Ikonsultan Tafs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TAFS IOSTests/TAFS_IOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  TAFS_IOSTests.swift
+//  TAFS IOSTests
+//
+//  Created by Moh Reza Luthfiansyah on 12/10/15.
+//  Copyright © 2015 Ikonsultan Tafs. All rights reserved.
+//
+
+import XCTest
+@testable import TAFS_IOS
+
+class TAFS_IOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
